f99444a00df40cf93aa72ddd410cf520c62d64dd
Giubis <116124169+Giubis@users.noreply.github.com>
2025-10-08 09:52:12 +0100

    chore: set server URL dynamic for local and Render environments

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 
 // Middlewares
 app.use(cors());
@@ -36,5 +37,5 @@ databaseConnection();
 
 // Server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${BASE_URL}`);
 });
