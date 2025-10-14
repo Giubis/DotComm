19b0c82399219c06369318eb200f22c61a84ea61
Giubis <116124169+Giubis@users.noreply.github.com>
2025-10-14 14:58:06 +0100

    test: reduce token expiry to 1m

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -162,7 +162,7 @@ const loginUser = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1h" }
+      { expiresIn: "1m" } // TEST
     );
 
     const validUser = {
